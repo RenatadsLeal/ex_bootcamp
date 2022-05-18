c6396b59e39bb33f4552723cd21d209945cd3258
clara <boclara@hotmail.com>
2022-05-18 11:15:04 -0300

    outro conflito criado

--- arquivo_conflito.go
+++ arquivo_conflito.go
@@ -1,5 +1,7 @@
+//Teste
 //Mexido - Clara
 //Vamos mexer nesse pra criar o conflito pedido no item 7 da atividade
 //Teste
 //Vamos mexer nesse pra criar o conflito pedido no item 7 da atividade
 // Criando conflitos propositalmente
+//Teste
